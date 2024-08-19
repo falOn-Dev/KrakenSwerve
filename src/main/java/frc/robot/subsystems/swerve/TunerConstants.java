c3fe6c35756bc696d33e18e5e0f93af56e7bf145
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-08-19 18:45:21 -0400

    Fixed kA Gain

--- src/main/java/frc/robot/subsystems/swerve/TunerConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/TunerConstants.java
@@ -16,8 +16,8 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-            .withKP(5).withKI(0.0).withKD(0.0)
-            .withKS(-0.016311).withKV(2.5182).withKA(0.2187);
+            .withKP(25).withKI(0.0).withKD(0.0)
+            .withKS(0).withKV(0).withKA(0);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
