836c5f6f2b9b7fef7c7ea0f73003a676c4ca533b
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-08-28 17:35:39 -0400

    Update GeomUtil.kt

--- src/main/java/lib/GeomUtil.kt
+++ src/main/java/lib/GeomUtil.kt
@@ -1,8 +1,11 @@
 package lib
 
 import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Pose3d
 import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.geometry.Rotation3d
 import edu.wpi.first.math.geometry.Translation2d
+import edu.wpi.first.math.geometry.Translation3d
 import kotlin.math.abs
 
 fun Pose2d.near(other: Pose2d, epsilon: Double = 1E-9): Boolean {
@@ -20,3 +23,17 @@ fun Translation2d.near(other: Translation2d, epsilon: Double = 1E-9): Boolean {
 fun Double.near(other: Double, epsilon: Double = 1E-9): Boolean {
     return abs(this - other) < epsilon
 }
+
+fun Pose2d.flip() = Pose2d(this.translation.flip(), this.rotation.flip())
+
+fun Translation2d.flip() = Translation2d(16.54 - this.x, this.y)
+
+fun Rotation2d.flip(): Rotation2d = this.rotateBy(Rotation2d.fromDegrees(180.0))
+
+fun Pose3d.flip() = Pose3d(this.translation.flip(), this.rotation.flip())
+
+fun Translation3d.flip() = Translation3d(16.54 - this.x, this.y, this.z)
+
+fun Rotation3d.flip(): Rotation3d = this.rotateBy(Rotation3d(0.0, 0.0, Math.PI))
+
+fun Gamepiece.flip() = Gamepiece(this.first.flip(), this.second)
